8aac282612cecc91148a116f2cfaba7b1f925d38
ytsuneo <yamamototsuneo@hotmail.com>
2020-08-28 17:25:55 -0300

    primeiro commit

---
+++ fundamentos/organizacao-basica.js
@@ -0,0 +1,7 @@
+console.log('eu sou uma sentença em javascript')
+
+{
+    console.log('eu sou um bloco de codigo em javascript')
+    console.log('eu sou um outro bloco de codigo em javascript')
+
+}
